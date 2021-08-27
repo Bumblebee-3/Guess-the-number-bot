64aacd88f840bec1a67d1c2f7167ddb384672c4f
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2021-08-27 11:51:11 +0530

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -2,7 +2,7 @@ module.exports = {
   name:"help",
   code:`
   $title[Help!]
-  $description[**My Commands:**\n\`gtn-stats\`,\`gtn\`,\`guess-number\`,\`disable-gtn\`,\`prefix\`.]
+  $description[**My Commands:**\n\`$getServerVar[prefix]gtn-stats\`,\`$getServerVar[prefix]gtn\`,\`$getServerVar[prefix]guess-number\`,\`$getServerVar[prefix]disable-gtn\`,\`$getServerVar[prefix]prefix\`.]
   $color[RANDOM]
   $footer[Source Code By Bumblebee]
   `
